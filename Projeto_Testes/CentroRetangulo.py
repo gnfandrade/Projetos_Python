8d97eb1ff4a6a4b3516ababf3bf3207de1282488
Gustavo Nunes <gnfandrade@gmail.com>
2018-02-01 10:14:24 -0200

    Classe Retângulo testada via TDD pelo módulo TesteCentroRetangulo e adicionado funcionalidades para interação do usuário

--- Projeto_Testes/CentroRetangulo.py
+++ Projeto_Testes/CentroRetangulo.py
@@ -6,18 +6,55 @@ class Ponto(object):
         self.y = y
 
     def imprime(self):
-        return self.x, self.y
-        #print("Ponto ({},{})".format(self.x, self.y))
+        #return self.x, self.y
+        print("Ponto ({},{})".format(self.x, self.y))
 
 class Retangulo(object):
     def __init__(self, ponto = Ponto(), b = 0, h = 0):
         self.verticePartida = ponto
         self.largura = b
         self.altura = h
-pt = Ponto()
-ret = Retangulo(pt.imprime())
 
-print(ret.verticePartida)
+    def encontraCentro(self):
+        x = self.verticePartida.x + self.largura/2
+        y = self.verticePartida.y + self.altura/2
+
+        return Ponto(x, y)
 
 
+def main():
+    verticePartida = Ponto()
+    ret = Retangulo()
 
+    while True:
+        cmd = menu()
+
+        if cmd.startswith('m'):
+            mudaValores(ret)
+        elif cmd.startswith('i'):
+            ret.encontraCentro().imprime()
+        else:
+            break
+
+
+def menu():
+    while True:
+        cmd = input("Deseja alterar os valores do retângulo(m), imprimir o valor do centro(i) ou fechar o programa(f)?\n")
+        cmd.lower()
+
+        if not cmd.isalpha():
+            print("Digite apenas Letras!!!")
+        else:
+            if cmd.startswith('m') or cmd.startswith('i') or cmd.startswith('f'):
+                return cmd
+            else:
+                print("Não entendi o seu comando, por favor digite novamente!")
+
+def mudaValores(ret):
+    ret.largura = float(input("Digite o valor da largura do retângulo: "))
+    ret.altura = float(input("Digite o valor da altura do retângulo: "))
+    ret.verticePartida.x = float(input("Digite o valor de x do vértice do retângulo: "))
+    ret.verticePartida.y = float(input("Digite o valor de y do vértice do retângulo: "))
+
+if __name__ == '__main__':
+    main()
--- Projeto_Testes/TesteCentroRetangulo.py
+++ Projeto_Testes/TesteCentroRetangulo.py
@@ -4,24 +4,34 @@ from Projeto_Testes.CentroRetangulo import *
 
 class TestPonto(unittest.TestCase):
     def testPontoConstrutor(self):
-        pt = Ponto()
+        pt = Ponto(2, 3)
 
-        self.assertEqual(1, 1, pt.__init__())
+        self.assertEqual(2, pt.x)
+        self.assertEqual(3, pt.y)
 
     def testImprime(self):
-        pt = Ponto()
+        pt = Ponto(2, 4)
 
-        self.assertEqual((0, 0), pt.imprime())
+        self.assertEqual((2, 4), pt.imprime())
 
 
 class TestRetangulo(unittest.TestCase):
     def testRetanguloConstrutor(self):
-        pt = Ponto()
+        pt = Ponto(2, 4)
         ret = Retangulo(pt.imprime())
 
         self.assertEqual(0, ret.altura)
         self.assertEqual(0, ret.largura)
-        self.assertEqual((0, 0), ret.verticePartida)
+        self.assertEqual((2, 4), ret.verticePartida)
+
+    def testEncontraCentro(self):
+        pt = Ponto(1, 2)
+        ret = Retangulo(pt, 5, 6)
+
+        self.assertEqual((3, 4), ret.encontraCentro().imprime())
+
+
+
 
 
 if __name__ == '__main__':
