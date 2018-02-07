eeafeae40c6d5ae611c92e5fadd8b68eabd23d97
Gustavo Nunes <gnfandrade@gmail.com>
2018-02-07 08:23:02 -0200

    Modificando o Módulo Teste_ObjetoGrafico

--- Projeto_Testes/Teste_ObjetoGrafico.py
+++ Projeto_Testes/Teste_ObjetoGrafico.py
@@ -1,9 +1,20 @@
 import unittest
-
 from Projeto_Testes.Objeto_Grafico import *
-from Projeto_Testes.CentroRetangulo import *
 
 
 class TestObjetoGrafico(unittest.TestCase):
+    def TestConstructor(self):
+        og = ObjetoGrafico("Azul", True, "Azul Claro")
 
+        self.assertEqual("Azul", og.cor_preenchimento)
+        self.assertEqual(True, og.preenchido)
+        self.assertEqual("Azul Claro", og.cor_contorno)
+
+class TestRetangulo(unittest.TestCase):
+    def TestConstructor(self):
+        og = ObjetoGrafico()
+
+
+if __name__ == '__main__':
+    unittest.main()
 
