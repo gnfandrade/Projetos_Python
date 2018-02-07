7b251dffafb12a0b3e02d23c33c15b34896f87bd
Gustavo Nunes <gnfandrade@gmail.com>
2018-02-07 07:59:14 -0200

    Testando Classe Objeto Gráfico

---
+++ Projeto_Testes/Objeto_Grafico.py
@@ -0,0 +1,43 @@
+class ObjetoGrafico(object):
+    def __init__(self, cor_preenchimento, preenchido, cor_contorno):
+        self.cor_preenchimento = cor_preenchimento
+        self.preenchido = preenchido
+        self.cor_contorno = cor_contorno
+
+
+class Retangulo(ObjetoGrafico):
+    def __init__(self, cor_preenchimento, preenchido, cor_contorno, base, altura):
+        super().__init__(cor_preenchimento, preenchido, cor_contorno)
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return self.base * self.altura
+
+    def perimetro(self):
+        return 2 * (self.altura + self.base)
+
+
+class Triangulo(ObjetoGrafico):
+    def __init__(self, cor_preenchimento, preenchido, cor_contorno, base, altura):
+        super().__init__(cor_preenchimento, preenchido, cor_contorno)
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return (self.altura * self.altura) / 2
+
+    def perimetro(self):
+        pass
+
+from math import pi
+class Circulo(ObjetoGrafico):
+    def __init__(self, cor_preenchimento, preenchido, cor_contorno, raio):
+        super().__init__(cor_preenchimento, preenchido, cor_contorno)
+        self.raio = raio
+
+    def area(self):
+        return pi * self.raio * self.raio
+
+    def perimetro(self):
+        return 2 * pi * self.raio
---
+++ Projeto_Testes/Teste_ObjetoGrafico.py
@@ -0,0 +1,9 @@
+import unittest
+
+from Projeto_Testes.Objeto_Grafico import *
+from Projeto_Testes.CentroRetangulo import *
+
+
+class TestObjetoGrafico(unittest.TestCase):
+
+
